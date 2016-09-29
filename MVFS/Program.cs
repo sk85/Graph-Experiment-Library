deb82f21040e6f39d10f7f219d7b62638a780982
melon87 <jun.melon.87@gmial.com>
2016-09-29 17:45:18 +0900

    MFVSの方をちょっと更新

--- MVFS/Program.cs
+++ MVFS/Program.cs
@@ -11,11 +11,43 @@ namespace MVFS
     {
         static void Main(string[] args)
         {
+            int Num = 6, Dim = 3;
+
+            int[] distAry = new int[Num];
+            for (int i = 1; i < Num; i++)
+            {
+                distAry[i] = 1;
+            }
+            distAry[0] = 0;
+
+            bool f = true;
+            while (f)
+            {
+                for (int i = 0; i < Num; i++)
+                {
+                    if (distAry[i] < Dim)
+                    {
+                        distAry[i]++;
+                        break;
+                    }
+                    else
+                    {
+                        distAry[i] = 1;
+                    }
+                }
+
+                for (int i = 0; i < Num; i++) Console.Write(distAry[i]);
+                Console.Write('\n');
+                Console.ReadKey();
+                for (int i = 0; i < Num; i++) f = f && (distAry[i] == Dim);
+                f = !f;
+            }
+
+            return;
             // とりあえずn = 5まで
             for (int dim = 2; dim < 5; dim++)
             {
-                VertexSetSet VSS = new VertexSetSet();
-                VSS.Initialize(dim);
+                VertexSetSet VSS = new VertexSetSet(dim);
                 while (!VSS.IsContainsFVS())
                 {
                     VSS = VSS.GetNext();
@@ -26,18 +58,40 @@ namespace MVFS
         }
     }
 
-    class VertexSet : List<short>
+    class VertexSet
     {
-        // 要素数1の状態に初期化
-        public void Initialize()
+        public int Dim; // グラフの次元数
+        public int Num; // 要素数
+        public short[] Array;
+
+        // コンストラクタ(要素数１)
+        public VertexSet(int dim)
         {
-            Add(0);
+            // 要素数1の状態に初期化
+            Dim = dim;
+            Num = 1;
+            Array = new short[1];
+            Array[0] = 0;
         }
 
-        // 引数の条件のノードが存在するか
-        public bool IsExist(short node)
+        // 次のサイズを作るためのコンストラクタ
+        public VertexSet(VertexSet old, short value)
         {
-            return false; // TODO
+            Dim = old.Dim;
+            Num = old.Num + 1;
+            Array = new short[Num];
+            for (int i = 0; i < old.Num; i++)
+            {
+                Array[i] = old.Array[i];
+            }
+            Array[old.Num] = value;
+        }
+
+        // 引数の条件のノードアドレスを返す(0なら失敗)
+        private short DistAryToAddr(int[] distAry)
+        {
+
+            return 0; // TODO
         }
 
         // FVSであるかどうか
@@ -47,21 +101,54 @@ namespace MVFS
             // 幅優先及び深さ優先探索でループの存在を確認
             return false;
         }
+
+        // このVSから派生する次の要素数のVS群を返す
+        public IEnumerable<VertexSet> GetNextVSs()
+        {
+            int[] distAry = new int[Num];
+            for (int i = 1; i < Num; i++) distAry[i] = 1;
+            distAry[0] = 0;
+
+            bool f = true;
+            while (f)
+            {
+                for (int i = 0; i < Num; i++)
+                {
+                    if (distAry[i] < Dim)
+                    {
+                        distAry[i]++;
+                        break;
+                    }
+                    else
+                    {
+                        distAry[i] = 1;
+                    }
+                }
+
+                short newAddr = DistAryToAddr(distAry);
+                if (newAddr != 0) yield return new VertexSet(this, newAddr);
+
+                for (int i = 0; i < Num; i++) f = f && (distAry[i] == Dim);
+                f = !f;
+            }
+            yield return null;
+        }
     }
 
     class VertexSetSet : List<VertexSet>
     {
         // 以下の"要素数"はVSの要素数のこと(VSSのことではない)
 
+        public int Dim; // グラフの次元数
         public int Num; // VSの要素数
 
-        // 要素数1の状態に初期化
-        public void Initialize(int dim)
+        public VertexSetSet(int dim)
         {
+            // 要素数1の状態に初期化
+            Dim = dim;
             Num = 1;
-            var VS = new VertexSet();
-            VS.Initialize();
-            Add(VS);    // TODO
+            var VS = new VertexSet(dim);
+            Add(VS);
         }
 
         // FVSを含んでいるか
@@ -78,12 +165,16 @@ namespace MVFS
         public VertexSetSet GetNext()
         {
             Num++;
-            VertexSetSet VSS = new VertexSetSet();
+            VertexSetSet VSS = new VertexSetSet(Dim + 1);
             foreach(VertexSet VS in this)
             {
-
+                var newVSs = VS.GetNextVSs();
+                foreach(VertexSet newVS in newVSs)
+                {
+                    VSS.Add(newVS);
+                }
             }
-            return null;    //TODO
+            return VSS;
         }
     }
 }
