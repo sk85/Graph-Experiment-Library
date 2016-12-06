21ffb85659a33b13a09bbf727627d2b1737afe4b
sk87 <jun.melon.87@gmail.com>
2016-12-06 13:22:06 +0900

    前方隣接を表示する実験を書いた

---
+++ GraphExperimentLibraryForCS/Debug/TestCodes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Graph.Core;
+
+namespace Graph.Debug
+{
+    /// <summary>
+    /// デバッグ用に表示したりするコード類をここにまとめてあります。
+    /// ほんとは基本グラフクラスに内蔵しておいて#ifdef DEBUGでくくる、
+    /// みたいにした方がいいとは思うんだけどとりあえずで。
+    /// </summary>
+    static class TestCodes
+    {
+        /// <summary>
+        /// グラフの各頂点の隣接頂点をコンソールに出力します。
+        /// キューブ用。
+        /// </summary>
+        /// <param name="graph">対象のグラフ</param>
+        public static void ShowNeighbors(AGraph graph)
+        {
+            Console.WriteLine("グラフの各頂点の隣接頂点をコンソールに出力します。");
+            Console.WriteLine("1つの出発頂点ごとに止まるので、何かキーを押して進めてください。");
+            BinaryNode node = new BinaryNode(0);
+            for (UInt32 nodeID = 0; nodeID < graph.NodeNum; nodeID++)
+            {
+                node.ID = nodeID;
+                Console.WriteLine("----------------------------------------------");
+                Console.WriteLine(Tools.UIntToBinStr(node.Addr, 32, 4) + '\n');
+                for (int i = 0; i < graph.GetDegree(node); i++)
+                {
+                    Console.WriteLine(Tools.UIntToBinStr(((BinaryNode)graph.GetNeighbor(node, i)).Addr, 32, 4));
+                }
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// グラフの各頂点からの距離をコンソールに出力します。
+        /// キューブ用。
+        /// </summary>
+        /// <param name="graph">対象のグラフ</param>
+        public static void ShowAllDistances(AGraph graph)
+        {
+            Console.WriteLine("グラフの各頂点からの距離をコンソールに出力します。");
+            Console.WriteLine("1つの出発頂点ごとに止まるので、何かキーを押して進めてください。");
+            for (UInt32 node1 = 0; node1 < graph.NodeNum; node1++)
+            {
+                int[] array = graph.CalcAllDistanceBFS(new BinaryNode(node1));
+                for (UInt32 node2 = 0; node2 < graph.NodeNum; node2++)
+                {
+                    Console.WriteLine("d({0,2}, {1,2}) = {2}", node1, node2, array[node2]);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ GraphExperimentLibraryForCS/Debug/Tools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph.Debug
+{
+    static class Tools
+    {
+        /// <summary>
+        /// 数値を2進数列の文字列に変換します。
+        /// </summary>
+        /// <param name="bin">数値</param>
+        /// <param name="length">長さ</param>
+        /// <param name="interval">スペースを入れる間隔</param>
+        /// <returns>文字列化した二進数列</returns>
+        public static string UIntToBinStr(UInt32 bin, int length, int interval)
+        {
+            string str = "";
+            for (int i = 0; i < length; i++)
+            {
+                if (i % interval == 0) str = " " + str;
+                str = ((bin >> i) & 1).ToString() + str;
+            }
+            return str;
+        }
+    }
+}
